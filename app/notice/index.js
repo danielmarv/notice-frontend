982159d8ce1ec2e3a3774529f4cea132cbdaf463
Daniel Ntege <danientege785@gmail.com>
2023-09-05 14:42:56 +0300

    Initial commit

---
+++ app/notice/index.js
@@ -0,0 +1,11 @@
+    // to fetch data
+    export const getStaticProps = async () => {
+        const res = await fetch("<http://localhost:1337/api/jobs?populate=*>")
+        const data = await res.json()
+        
+        return {
+             props: {
+                  getNotices: data.data
+             }
+        }
+   }
