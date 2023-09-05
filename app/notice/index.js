ea7009551c92b519eeaa0a164b905adbac7109c3
Daniel Ntege <danientege785@gmail.com>
2023-09-05 14:46:29 +0300

    Update index.js

--- app/notice/index.js
+++ app/notice/index.js
@@ -1,6 +1,6 @@
     // to fetch data
     export const getStaticProps = async () => {
-        const res = await fetch("<http://localhost:1337/api/jobs?populate=*>")
+        const res = await fetch("<http://localhost:1337/api/notices?populate=*>")
         const data = await res.json()
         
         return {
