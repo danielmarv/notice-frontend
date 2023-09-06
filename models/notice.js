bd3e61802067d8bd3f063f1b84771316a3fbb88a
Daniel Ntege <danientege785@gmail.com>
2023-09-06 12:56:29 +0300

    Notice Schema

---
+++ models/notice.js
@@ -0,0 +1,32 @@
+import { Schema, model, models } from 'mongoose';
+
+const NoticeSchema = new Schema({
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  notice: {
+    type: String,
+    required: [true, 'Notice is required.'],
+  },
+  tag: {
+    type: String,
+    required: [true, 'Tag is required.'],
+  },
+  department: {
+    type: String,
+    required: [true, 'Department name  is required.'],
+  },
+  school: {
+    type: String,
+    required: [true, 'School name is required.'],
+  },
+  type: {
+    type: String,
+    required: [true, 'Type is required is required.'],
+  },
+});
+
+const Notice = models.Notice || model('Notice', NoticeSchema);
+
+export default Notice;
