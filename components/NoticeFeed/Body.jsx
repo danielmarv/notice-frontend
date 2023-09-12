aca97ac173ccc3ef4b24b9a7d48986f3fa40177d
Daniel Ntege <danientege785@gmail.com>
2023-09-12 14:26:56 +0300

    Update Body.jsx

--- components/NoticeFeed/Body.jsx
+++ components/NoticeFeed/Body.jsx
@@ -19,34 +19,37 @@ const NoticePage = () => {
     fetchNoticeData();
   }, []); 
     
-    const MAX_LENGTH = 250
+  const maxLength = 250;
 
     return ( 
      <>
           <section className="w-full px-4 py-24 mx-auto max-w-7xl md:w-4/5">
-               <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
+               <div className="flex flex-col py-4 space-y-4 md:space-y-0 md:flex-row mt-5 md:justify-center md:space-x-4">
                     {noticeData.map((notice) => (
                         <Link key={notice.key} href={'/notices/' + notice.id}>
-                          <div>
+                          <div className="flex flex-col md:flex-row bg-gray-100 rounded-lg space-x-4 md:w-1/2">
                              <h2 className="mb-2 text-xl font-bold leading-snug text-gray-900">
                                  <a href="#" className="text-gray-900 hover:text-purple-700">{notice.attributes.notice}</a>
                              </h2>
-                             <a className="flex items-center text-gray-700" href="#">
-                               <div className="avatar">
+                             <div className="flex items-center text-gray-700" href="#">
+                               <div className="avatar ">
                                   <img
-                                    className="flex-shrink-0 object-cover object-center w-12 h-12 rounded-full"
+                                    className="md:flex h-56 w-56 rounded-lg"
                                     src={`http://localhost:1337${notice.attributes.images.data.attributes.url}`}
                                     alt={"Photo of " + notice.attributes.description} 
                                    />
                                 </div>
-                               <div className="ml-2">
-                                   <p className="text-sm font-semibold text-gray-900">{notice.attributes.department}</p>
-                                   <p className="text-sm text-gray-600">{notice.attributes.email}</p>
-                                    <p className="mb-4 text-sm font-normal text-gray-600">
-                                      {notice.attributes.details}
-                                    </p>
+                                
+                               <div>
+                                  <p className="text-sm font-semibold text-gray-900">{notice.attributes.department}</p>
+                                  <p className="text-sm text-gray-600">{notice.attributes.email}</p> 
                                </div>
-                             </a>
+                             </div>
+                              <div>
+                                <p className="mb-4 text-sm font-normal text-gray-600 mt-4">
+                                  { notice.attributes.details}
+                                </p>
+                              </div>
                            </div>
                         </Link>
                     ))}   
