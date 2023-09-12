bf35b538a6feb2ca6a963b004ba480b0b6f356ca
Daniel Ntege <danientege785@gmail.com>
2023-09-12 11:52:52 +0300

    Update Body.jsx

--- components/NoticeFeed/Body.jsx
+++ components/NoticeFeed/Body.jsx
@@ -1,18 +1,23 @@
-import Link from 'next/link'
+"use client";
+
+import React, { useState, useEffect} from 'react';
+import Link from 'next/link';
 
 
-export const getStaticProps = async () => {
-  const res = await fetch("<http://localhost:1337/api/notices?populate=*>")
-  const data = await res.json()
-  
-  return {
-    props: {
-        getNotices: data.data
-    }
-  }
-}
+const NoticePage = () => {
+  const [noticeData, setNoticeData] = useState([]);
 
-const NoticePage = ({getNotices}) => {
+  const fetchNoticeData = async () => {
+    const res = await fetch("http://localhost:1337/api/notices?populate=*");
+    const data = await res.json();
+    const getNotices = data.data || [];
+
+    setNoticeData(getNotices);
+  };
+
+  useEffect(() => {
+    fetchNoticeData();
+  }, []); 
     
     const MAX_LENGTH = 250
 
@@ -20,14 +25,14 @@ const NoticePage = ({getNotices}) => {
      <>
           <section className="w-full px-4 py-24 mx-auto max-w-7xl md:w-4/5">
                <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
-                    {getNotices.map( notice => (
+                    {noticeData.map((notice) => (
                         <Link key={notice.key} href={'/notices/' + notice.id}>
                           <div>
                              <h2 className="mb-2 text-xl font-bold leading-snug text-gray-900">
                                  <a href="#" className="text-gray-900 hover:text-purple-700">{notice.attributes.notice}</a>
                              </h2>
                                  <p className="mb-4 text-sm font-normal text-gray-600">
-                                    {notice.attributes.details.substring(0, MAX_LENGTH) + " ..."}
+                                    {notice.attributes.details}
                                   </p>
                              <a className="flex items-center text-gray-700" href="#">
                                <div className="avatar">
@@ -49,7 +54,8 @@ const NoticePage = ({getNotices}) => {
      
              {/* btns */}
              <div className="flex flex-col items-center justify-center mt-20 space-x-0 space-y-2 md:space-x-2 md:space-y-0 md:flex-row">
-                 <Link href="/"><a href="#" className="px-3 py-2 text-indigo-500 border border-indigo-500 border-solid hover:text-black md:w-auto">Home</a>
+                 <Link href="/" className="px-3 py-2 text-indigo-500 border border-indigo-500 border-solid hover:text-black md:w-auto">
+                    Home
                  </Link>
              </div>
          </section>     
