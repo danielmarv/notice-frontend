93f90d059225cdfc6c5064e238cd63f90e8888f9
Daniel Ntege <danientege785@gmail.com>
2023-09-13 12:44:36 +0300

    Notice details page

--- app/notice/page.jsx
+++ app/notice/page.jsx
@@ -1,49 +1,69 @@
-const DetailedNotices = ({notice}) => {
-  const Max_DATE_LENGHT = 10
-  const Max_TIME_LENGHT = 11
+"use client";
+
+import React, { useState, useEffect} from 'react';
+import Link from 'next/link';
+
+
+const NoticeList = () => {
+  const [noticeData, setNoticeData] = useState([]);
+
+  const fetchNoticeData = async () => {
+    const res = await fetch("http://localhost:1337/api/notices?populate=*&sort[0]=createdAt:desc&sort[1]=status");
+    const data = await res.json();
+    const getNotices = data.data || [];
+
+    setNoticeData(getNotices);
+  };
+
+  useEffect(() => {
+    fetchNoticeData();
+  }, []); 
   
-  return ( 
-      <>
-      {/* {JSON.stringify(notice, null, 2)} */}
-      <article className="px-4 py-5 mx-auto max-w-7xl">
-          <div className="w-full mx-auto mb-12 text-left md:w-3/4 lg:w-1/2">
-              <p className="mt-6 mb-2 text-xs font-semibold tracking-wider uppercase text-primary">{notice.attributes.categories.data[0].attributes.type + " LEVEL"}</p>
-              <h1 className="mb-3 text-3xl font-bold leading-tight text-gray-900 md:text-4xl">
-              {notice.attributes.vacancy}
-              </h1>
-              <div className="flex mb-6 space-x-2 text-sm">
-              <a className="p-1 bg-indigo-500 rounded-full text-gray-50 badge hover:bg-gray-200" href="#">CSS</a>
-              <a className="p-1 bg-indigo-500 rounded-full text-gray-50 badge hover:bg-gray-200" href="#">Tailwind</a>
-              <a className="p-1 bg-indigo-500 rounded-full text-gray-50 badge hover:bg-gray-200" href="#">AlpineJS</a>
-              </div>
-              <a className="flex items-center text-gray-700" href="#">
-              <div className="avatar">
-                  <img 
-                  className="flex-shrink-0 object-cover object-center w-24 h-24 rounded-full"
-                  src={`http://localhost:1337${notice.attributes.image.data.attributes.url}`}
-                  alt={"Photo of " + notice.attributes.recruiter} />
-              </div>
-              <div className="ml-4">
-                  <p className="font-semibold text-gray-800 text-md">{notice.attributes.recruiter}</p>
-                  <p className="text-sm text-gray-500">{notice.attributes.publishedAt.substring(0, Max_DATE_LENGHT)}</p>
-                  <p className="text-sm text-gray-500">{notice.attributes.createdAt.substring(16, Max_TIME_LENGHT)}</p>
-              </div>
-              </a>
-          </div>
+    
+  const maxLength = 250;
 
-          <div className="w-full mx-auto prose md:w-3/4 lg:w-1/2">
-              <p>
-              {notice.attributes.description}
-              </p>
-          </div>
-          
-          <div className="flex flex-col items-center justify-center mt-10 space-x-0 space-y-2 md:space-x-2 md:space-y-0 md:flex-row">
-               <Link href="/notices"><a href="" className="px-3 py-2 text-indigo-500 border border-indigo-500 border-solid hover:text-black md:w-auto">Back</a>
-               </Link>
-           </div>
-      </article>
-      </>
-   )
+  return (
+    <>
+      <section className="w-full px-2 py-4 mx-auto max-w-7xl md:w-4/5">
+        <div className="flex flex-col py-4  md:space-y-0 mt-5">
+          {noticeData.map((notice) => (
+            <Link key={notice.id} href={`/notice/${notice.id}`}>
+              <div className="flex flex-col md:flex-row bg-gray-100 rounded-lg pt-4">
+                <div className="md:w-1/2">
+                  <div className="avatar">
+                    <img
+                      className="md:flex h-70 w-70 rounded-lg"
+                      src={`http://localhost:1337${notice.attributes.images.data.attributes.url}`}
+                      alt={"Photo of " + notice.attributes.description}
+                    />
+                  </div>
+                </div>
+  
+                <div className="md:w-1/2 p-4">
+                  <h2 className="mb-2 text-xl font-bold leading-snug text-gray-900">
+                    <a href="#" className="text-gray-900 hover:text-purple-700">{notice.attributes.notice}</a>
+                  </h2>
+                  <div className="flex items-center text-gray-700" href="#">
+                    <div>
+                      <p className="text-sm font-semibold text-gray-900">{notice.attributes.department}</p>
+                      <p className="text-sm font-semibold text-gray-600">{notice.attributes.email}</p>
+                      <p className="text-sm font-semibold text-gray-600">{notice.attributes.venue}</p>
+                    </div>
+                  </div>
+                  <div>
+                    <p className="mb-4 text-sm font-normal text-gray-600 mt-4">
+                      {notice.attributes.description}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+  
 }
-
-export default DetailedNotices;
+ 
+export default NoticeList;
