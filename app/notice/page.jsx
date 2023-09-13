f6ef562cfa2fcf97749e88eeed1b66f9910b664e
Daniel Ntege <danientege785@gmail.com>
2023-09-13 10:01:14 +0300

    id details for the notice

--- app/notice/page.jsx
+++ app/notice/page.jsx
@@ -1,51 +1,49 @@
-"use client";
+const DetailedNotices = ({notice}) => {
+  const Max_DATE_LENGHT = 10
+  const Max_TIME_LENGHT = 11
+  
+  return ( 
+      <>
+      {/* {JSON.stringify(notice, null, 2)} */}
+      <article className="px-4 py-5 mx-auto max-w-7xl">
+          <div className="w-full mx-auto mb-12 text-left md:w-3/4 lg:w-1/2">
+              <p className="mt-6 mb-2 text-xs font-semibold tracking-wider uppercase text-primary">{notice.attributes.categories.data[0].attributes.type + " LEVEL"}</p>
+              <h1 className="mb-3 text-3xl font-bold leading-tight text-gray-900 md:text-4xl">
+              {notice.attributes.vacancy}
+              </h1>
+              <div className="flex mb-6 space-x-2 text-sm">
+              <a className="p-1 bg-indigo-500 rounded-full text-gray-50 badge hover:bg-gray-200" href="#">CSS</a>
+              <a className="p-1 bg-indigo-500 rounded-full text-gray-50 badge hover:bg-gray-200" href="#">Tailwind</a>
+              <a className="p-1 bg-indigo-500 rounded-full text-gray-50 badge hover:bg-gray-200" href="#">AlpineJS</a>
+              </div>
+              <a className="flex items-center text-gray-700" href="#">
+              <div className="avatar">
+                  <img 
+                  className="flex-shrink-0 object-cover object-center w-24 h-24 rounded-full"
+                  src={`http://localhost:1337${notice.attributes.image.data.attributes.url}`}
+                  alt={"Photo of " + notice.attributes.recruiter} />
+              </div>
+              <div className="ml-4">
+                  <p className="font-semibold text-gray-800 text-md">{notice.attributes.recruiter}</p>
+                  <p className="text-sm text-gray-500">{notice.attributes.publishedAt.substring(0, Max_DATE_LENGHT)}</p>
+                  <p className="text-sm text-gray-500">{notice.attributes.createdAt.substring(16, Max_TIME_LENGHT)}</p>
+              </div>
+              </a>
+          </div>
 
-import { useState } from "react";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+          <div className="w-full mx-auto prose md:w-3/4 lg:w-1/2">
+              <p>
+              {notice.attributes.description}
+              </p>
+          </div>
+          
+          <div className="flex flex-col items-center justify-center mt-10 space-x-0 space-y-2 md:space-x-2 md:space-y-0 md:flex-row">
+               <Link href="/notices"><a href="" className="px-3 py-2 text-indigo-500 border border-indigo-500 border-solid hover:text-black md:w-auto">Back</a>
+               </Link>
+           </div>
+      </article>
+      </>
+   )
+}
 
-import Form from "@components/Form";
-
-const CreatePrompt = () => {
-  const router = useRouter();
-  const { data: session } = useSession();
-
-  const [submitting, setIsSubmitting] = useState(false);
-  const [post, setPost] = useState({ prompt: "", tag: "" });
-
-  const createPrompt = async (e) => {
-    e.preventDefault();
-    setIsSubmitting(true);
-
-    try {
-      const response = await fetch("/api/prompt/new", {
-        method: "POST",
-        body: JSON.stringify({
-          prompt: post.prompt,
-          userId: session?.user.id,
-          tag: post.tag,
-        }),
-      });
-
-      if (response.ok) {
-        router.push("/");
-      }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsSubmitting(false);
-    }
-  };
-
-  return (
-    <Form
-      type='Create'
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={createPrompt}
-    />
-  );
-};
-
-export default CreatePrompt;
+export default DetailedNotices;
