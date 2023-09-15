fb75c5a7d6c89a4eb8708b8b9e91d03a4c177ec4
Daniel Ntege <danientege785@gmail.com>
2023-09-15 14:59:58 +0300

    commit

--- components/Newsletter.jsx
+++ components/Newsletter.jsx
@@ -7,8 +7,17 @@ const SubscriptionForm = () => {
   const [successMessage, setSuccessMessage] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    
+    if (!email || !email.match(/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/)) {
+      setErrorMessage('Please enter a valid email address.');
+      return;
+    }  
 
     const data = {
       data: {
@@ -19,11 +28,11 @@ const SubscriptionForm = () => {
     };
     
     try {
-      const response = await fetch('http://localhost:1337/api/subscription', {
+      const response = await fetch('http://localhost:1337/api/subscriptions', {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
+        // headers: {
+        //   'Content-Type': 'application/json',
+        // },
         body: JSON.stringify(data),
       });
 
@@ -37,7 +46,7 @@ const SubscriptionForm = () => {
       setErrorMessage('An error occurred. Please try again later.');
     }
 
-    // setEmail('');
+    setEmail('');
   };
 
 
@@ -62,8 +71,8 @@ const SubscriptionForm = () => {
               <input
                 type="Email"
                 name="email"
-                value={data.email}
-                onChange={(e) => setEmail({ ...data, email: e.target.value })}
+                value={email}
+                onChange={handleEmailChange}
                 autoComplete="email"
                 required
                 className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
