2d804bed4899e2345fc1df578d1a2f7a60e57630
Daniel Ntege <danientege785@gmail.com>
2023-10-28 14:21:23 +0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -4,13 +4,13 @@ import NoticePage from "../components/NoticeFeed/Body";
 const Home = () => (
   <section className='flex-col w-full flex-center'>
     <h1 className='text-center head_text'>
-      Online Notice Board
+      Bugema University Notice Board
       <br className='max-md:hidden' />
-      <span className='text-center orange_gradient'> AI-Powered Notice Board</span>
+      <span className='text-center orange_gradient'>Communication Eased Through Tech</span>
     </h1>
     <p className='text-center desc'>
       This is an Online Notice Board System designed to improve on the communication
-      in our Community
+      in our Campus
     </p>
     <NoticePage />
     
